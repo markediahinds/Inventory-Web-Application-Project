6606277aae790980da0ca0b21d18b7455e815fb4
Markedia Hinds <mhinds@pursuit.org>
2024-03-23 18:47:56 -0400

    Project in Progress

---
+++ script.js
@@ -0,0 +1,11 @@
+// function changeColor()
+//     {
+//     alert("bla")
+//    document.getElementById("text1").style("backgroundColor":"red"); 
+//     }
+
+
+
+    let button = document.querySelector("button");
+    let input = document.querySelectorAll("input");
+    let bookList = document.querySelector(("ul"));
